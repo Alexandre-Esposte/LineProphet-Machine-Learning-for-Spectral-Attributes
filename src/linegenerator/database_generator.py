014d16d4c7cdace299ac16aaa7228ff5b93549f9
Alexandre Esposte <alexandreesposte@gmail.com>
2024-04-23 13:29:29 -0300

    db gerado

---
+++ src/linegenerator/database_generator.py
@@ -0,0 +1,27 @@
+from line_generator import voigt_profile_generator
+import pandas as pd
+
+
+def database_generator(samples: int):
+
+    db = {'sigma':[],'gamma':[],'fwhm':[],'wv_mean':[],'wv_std':[], 'int_mean':[],'int_std':[],'int_max':[]}
+    for _ in range(samples):
+
+        profile = voigt_profile_generator()
+        print(_)
+        db['sigma'].append(profile['sigma'])
+        db['gamma'].append(profile['gamma'])
+        db['fwhm'].append(profile['fwhm'])
+        db['wv_mean'].append(profile['statistics']['wv_mean'])
+        db['wv_std'].append(profile['statistics']['wv_std'])
+        db['int_mean'].append(profile['statistics']['int_mean'])
+        db['int_std'].append(profile['statistics']['int_std'])
+        db['int_max'].append(profile['statistics']['int_max'])
+
+    db = pd.DataFrame(db)
+    db.to_csv('././database/line_profiles.csv',index=None)
+
+
+if __name__ == "__main__":
+    database_generator(1000)
+    print('Done')
